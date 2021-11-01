e05a4147c4f3143b0d46bea2ef87f924a5f315cf
Angel Martinez <angel.martineztrc@gmail.com>
2021-10-31 19:32:14 -0500

    feat:  initialize a express application

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+import express from 'express';
+
+const app = express();
+
+app.listen(5000, '0.0.0.0', () => {
+  console.log(`Listening at http://localhost:5000`);
+});
