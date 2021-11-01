5e97221826b4793c0eb521096f2cf658f7ae740a
Angel Martinez <angel.martineztrc@gmail.com>
2021-10-31 19:39:26 -0500

    chore:  add dotenv for env variables

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, '0.0.0.0', () => {
+app.listen(Number(process.env.PORT), '0.0.0.0', () => {
   console.log(`Listening at http://localhost:5000`);
 });
